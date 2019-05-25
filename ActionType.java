3c5b1ed800313537b5a8b0d89a9cd98ba0bf0ac2
mojtaba-yekta <mojtaba-yekta@users.noreply.github.com>
2019-05-25 12:17:32 +0430

    creating action type file
    creating file

---
+++ ActionType.java
@@ -0,0 +1,10 @@
+package com.mojtaba.helper.pojomodel;
+
+import com.google.gson.annotations.SerializedName;
+
+public enum ActionType {
+    @SerializedName("0")
+    IsLogin ,
+    @SerializedName("1")
+    Logout ,
+}
